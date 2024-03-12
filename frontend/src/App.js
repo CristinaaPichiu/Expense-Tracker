ce926dc1f3f4e5868200075272b1e4884a51b0c8
Cristina Pichiu <cristinapichiu38@gmail.com>
2024-03-12 23:36:24 +0200

    finished back + form front

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from "styled-components";
+import bg from './img/bg.png'
+import {MainLayout} from './styles/Layouts'
+import Orb from './Components/Orb/Orb'
+import Navigation from './Components/Navigation/Navigation';
+import { useState, useMemo } from 'react';
+import React from 'react'
+
+
+
+
+
+function App() {
+  const [active, setActive] = useState(1)
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  },[])
+
+  return (
+    <AppStyled bg={bg} className="App">
+      {orbMemo}
+      <MainLayout>
+        <Navigation active={active} setActive={setActive} />
+      </MainLayout>
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+ 
+`;
+
+
+export default App;
