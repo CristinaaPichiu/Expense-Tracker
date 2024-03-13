4d724a6bf2007f01bdb691791e390647ed9e683a
Cristina Pichiu <cristinapichiu38@gmail.com>
2024-03-14 01:36:10 +0200

    front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,35 @@ import bg from './img/bg.png'
 import {MainLayout} from './styles/Layouts'
 import Orb from './Components/Orb/Orb'
 import Navigation from './Components/Navigation/Navigation';
-import { useState, useMemo } from 'react';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Income from './Components/Income/Income'
+import Expenses from './Components/Expenses/Expenses';
+import { useState, useMemo, useCallback } from 'react';
 import React from 'react'
-
-
-
+import { useGlobalContext } from './context/globalContext';
 
 
 function App() {
   const [active, setActive] = useState(1)
+
+  
+  const global = useGlobalContext()
+  console.log(global)
+
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
   const orbMemo = useMemo(() => {
     return <Orb />
   },[])
@@ -23,6 +43,10 @@ function App() {
       {orbMemo}
       <MainLayout>
         <Navigation active={active} setActive={setActive} />
+        <main>
+        {displayData()}
+
+        </main>
       </MainLayout>
     </AppStyled>
   );
@@ -32,6 +56,16 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
   position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
  
 `;
 
